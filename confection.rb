167374ab2e722b98ab92096fbd1f2d0ce8b76b79
ianrebulado <eilrebulado@gmail.com>
2024-01-27 09:07:54 +0800

    confection.rb assignment

---
+++ confection.rb
@@ -0,0 +1,14 @@
+class Confection
+  def bake
+    p "Baking at 350 degrees for 25 minutes."
+  end 
+end
+
+class BananaCake > Confection
+end
+
+class Cupcakes < Confection
+  def frosting
+    p "Apply frosting"
+  end
+end
