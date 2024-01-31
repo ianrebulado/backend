9f6a923a9b8fea5c4495f9c89f184193e580356f
ianrebulado <eilrebulado@gmail.com>
2024-02-01 06:25:45 +0800

    chore: add isogram activity

---
+++ isogram.rb
@@ -0,0 +1,14 @@
+def is_isogram(word)
+  word = word.downcase
+  uniq_chars = Set.new
+
+  word.each_char do |char|
+    if uniq_chars.include?(char)
+      puts false
+    return
+    else
+    uniq_chars.add(char)
+    end
+  end
+  puts true
+end
