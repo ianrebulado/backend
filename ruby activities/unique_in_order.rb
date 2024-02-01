69c3fab09347667872a9258470ece0056981ad64
ianrebulado <eilrebulado@gmail.com>
2024-02-01 17:57:41 +0800

    refactor: add method for array sequence

--- ruby activities/unique_in_order.rb
+++ ruby activities/unique_in_order.rb
@@ -1,9 +1,15 @@
 def unique_in_order(seq)
     order = []
+  if seq.is_a?(String)
     seq.each_char do |char|
       order << char unless order.last == char 
     #   order[-1] ??
     end
+  else
+    seq.each_with_index do |item, index|
+      order << item unless index > 0 && seq[index - 1] == item
+    end
+  end
     order
 end
   
