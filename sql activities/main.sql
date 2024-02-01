f8d24599a1d04fac8a4bc2e7a4a67691229b4b1b
ianrebulado <eilrebulado@gmail.com>
2024-02-01 14:47:22 +0800

    chore: add research paper activity

--- sql activities/main.sql
+++ sql activities/main.sql
@@ -43,24 +43,26 @@ ORDER BY age DESC;
 
 -- ACTIVITY 2
 
+DROP TABLE classrooms
+
 CREATE TABLE classrooms(
 id              integer     PRIMARY KEY,
 student_id      integer     NOT NULL,
-section         character varying(50) NOT NULL
+section         VARCHAR(1)  NOT NULL
 );
 
 INSERT INTO classrooms (id, student_id, section)
 VALUES
-(1, 1, "A"),
-(2, 2, "A"),
-(3, 3, "B"),
-(4, 4, "C"),
-(5, 5, "B"),
-(6, 6, "A"),
-(7, 7, "C"),
-(8, 8, "B"),
-(9, 9, "B"),
-(10, 10, "C");
+(1, 1, 'A'),
+(2, 2, 'A'),
+(3, 3, 'B'),
+(4, 4, 'C'),
+(5, 5, 'B'),
+(6, 6, 'A'),
+(7, 7, 'C'),
+(8, 8, 'B'),
+(9, 9, 'B'),
+(10, 10, 'C');
 
 SELECT * FROM students;
 SELECT * FROM classrooms;
@@ -80,3 +82,51 @@ ON s.id = c.student_id;
 SELECT * FROM students s
 FULL JOIN classrooms c
 ON s.id = c.student_id;
+
+
+-- 
+
+-- DROP TABLE research_papers
+-- DROP TYPE VALID_GRADE
+
+
+CREATE TYPE VALID_GRADE AS ENUM ('A', 'B', 'C', 'D', 'E', 'F');
+
+CREATE TABLE research_papers(
+id              INT                     PRIMARY KEY,
+student_id      INT                     NOT NULL,
+grade           VALID_GRADE,
+
+FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+INSERT INTO research_papers(id, student_id, grade)
+VALUES
+(1, 1, 'A'),
+(2, 1, 'B'),
+(3, 2, 'C'),
+(4, 2, 'D'),
+(5, 3, 'F'),
+(6, 4, 'A'),
+(7, 5, 'A'),
+(8, 5, 'F'),
+(9, 4, NULL),
+(10, 5, NULL);
+
+-- SELECT * FROM research_papers
+
+
+-- QUERY ALL STUDENTS W MULTIPLE PAPERS
+SELECT s.first_name first_name, s.last_name last_name, COUNT(r.id) papers
+FROM students s
+INNER JOIN research_papers r ON s.id = r.student_id
+GROUP BY s.id
+HAVING COUNT(r.id) > 1
+
+-- QUERY ALL STUDENTS WITH UNGRADED PAPERS
+SELECT s.first_name first_name, s.last_name last_name,
+       r.id paper_id, r.grade grade
+FROM students s
+    INNER JOIN research_papers r ON s.id = r.student_id
+WHERE r.grade IS NULL;
+
